8d588f9c940a82b44cc5cd9d8b1aef7e533f4eec
Neetika Bhandari <shivanjali907@gmail.com>
2022-09-30 17:11:59 +0530

    demo_ navigation_bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+
+  # root to: 'pages#home'
+
+  root 'pages#index'
+  get 'about', to: 'pages#about'
+
 end
